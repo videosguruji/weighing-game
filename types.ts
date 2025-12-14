92dae05b70ee1a15517f25ae22c8c5fc8444174b
videosguruji <videosguruji@gmail.com>
2025-12-14 09:58:06 +0530

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,9 +1,12 @@
-
 import React from 'react';
 
-export interface Fruit {
+export type ItemType = 'fruit' | 'weight';
+
+export interface Item {
   id: string;
   name: string;
-  weight: number;
-  Icon: React.FC<{ className?: string }>;
+  weight: number; // in kg
+  type: ItemType;
+  // Use a more generic prop type that includes className, compatible with both SVG and HTML elements
+  Icon: React.FC<{ className?: string } & React.SVGProps<SVGSVGElement>>;
 }
